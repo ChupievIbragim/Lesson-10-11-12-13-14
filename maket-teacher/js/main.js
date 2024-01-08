5a7a487f583f2c47c7d34d665887d2ec39160f83
IbragimBIN21 <153689931+IbragimBIN21@users.noreply.github.com>
2024-01-08 16:54:39 +0300

    Add files via upload

--- maket-teacher/js/main.js
+++ maket-teacher/js/main.js
@@ -24,9 +24,52 @@ window.addEventListener('DOMContentLoaded', function() {
       });
     });
 
+    $( function() {
+      $( "#accordion" ).accordion();
+    } );
+
     // burger
     document.querySelector('.header__burger').addEventListener('click', function() {
-      document.querySelector('.header-nav').classList.toggle('nav-content-active');
+      document.querySelector('.header-nav').classList.toggle('header-nav-active')
       })
+      var headerBurger = document.querySelector('.header__burger');
 
+      headerBurger.addEventListener('click', function() {
+        headerBurger.classList.toggle('active');
+      });
+      document.querySelector('.header__burger').addEventListener('click', function() {
+        document.querySelector('.header__burger-line').classList.toggle('active')
+        })
+
+        // document.getElementById('toggleButton').addEventListener('click', function() {
+        //   var image1 = document.getElementById('image1');
+        //   var image2 = document.getElementById('image2');
+        
+        //   // Переключаем свойство display для скрытия/отображения изображений
+        //   if (image1.style.display === 'block') {
+        //     image1.style.display = 'none';
+        //     image2.style.display = 'block';
+        //   } else {
+        //     image1.style.display = 'block';
+        //     image2.style.display = 'none';
+        //   }
+        // });
+        // document.getElementById('toggleButton').addEventListener('click', function() {
+        //   document.querySelector('.main-header').classList.toggle('search-input')
+        // })
+
+        // var toggleButton = document.getElementById("toggleButton");
+
+        // // Привязываем обработчик события к кнопке
+        // toggleButton.addEventListener("click", function() {
+        //   var elements = document.getElementsByClassName("search-ticket-item");
+
+        //   for (var i = 0; i < elements.length; i++) {
+        //     var element = elements[i];
+        //     if (element.style.display === "none" || element.style.display === "") {
+        //       element.style.display = "block";
+        //     } else {
+        //       element.style.display = "none";
+        //     }
+        //   }
 });
